808a3ff440fe1ebcbe9f4e304ed8aa0534c36925
jujuprograma <julia.2023303012@aluno.iffar.edu.br>
2025-05-08 16:52:12 -0300

    Update salvar.php

--- j/salvar.php
+++ j/salvar.php
@@ -1,11 +1,17 @@
 <?php
-include("conexao.php");
-$nome=$_POST["Nome"];
-$endereco=$_POST["Endereco"];
-$fone=$_POST["Telefone"];
-$sql="INSERT INTO Agenda.Contatos (nome, endereco, telefone)
-VALUES ('$nome', '$endereco', '$fone')";
-if (mysqli_query($conexao, $sql)){
-    echo "contato salvo!";
-}else "erro ao salvar".mysqli_error($conexao);
+include('conexao.php');
+
+$nome = $_POST['nome'];
+$endereco = $_POST['endereco'];
+$telefone = $_POST['telefone'];
+
+$sql = "INSERT INTO contatos (nome, endereco, telefone) VALUES ('$nome', '$endereco',
+'$telefone')";
+
+if (mysqli_query($conexao, $sql)) {
+    echo "Contato salvo com sucesso!";
+    echo "<br><a href='index.php'>Voltar</a>";
+} else {
+    echo "Erro ao salvar: " . mysqli_error($conexao);
+}
 ?>
