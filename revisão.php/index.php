9d43d11726ddb215bbf25a34cbfed2ce5476a2fe
jujuprograma <julia.2023303012@aluno.iffar.edu.br>
2025-06-12 16:43:34 -0300

    Add files via upload

---
+++ revisão.php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+</head>
+<body>
+    <h1> Identifique-se! </h1>
+    <form Method="GET" Action="pagina_protegida.php">
+    Usuário: <input type="text" name="nome" placeholder="Nome de usuário aqui">
+    <br><br>
+    Senha: <input type="password" name="senha" placeholder= "Digite a senha aqui">
+    <br><br>
+    <input type=submit value="Enviar">
+</form>
+</body>
+</html>
---
+++ revisão.php/pagina_protegida.php
@@ -0,0 +1,4 @@
+<?php
+$_nome=$_GET["nome"];
+$_senha=$_GET["senha"];
+Echo "Boas vindas";?>
